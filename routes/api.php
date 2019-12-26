438f6394fbf3e7adfc12d80f04b27efed76c9f8c
Yahya <yahyatinani@gmail.com>
2019-12-26 22:56:16 +0100

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,19 @@
 <?php
+
+$postRoutes = array(
+    'cities' => 'AddCityController',
+    'cities/{id}/delivery-times' => 'AddCityDeliveryTimeController',
+    'cities/{id}/excluded-delivery-times' => 'ExcludeCityDeliveryTimesController',
+    'cities/{id}/exclude-delivery-date' => 'ExcludeCityDeliveryDateController',
+    'delivery-times' => 'AddDeliveryTimeController',
+);
+
+foreach ($postRoutes as $path => $controller)
+    addPostPath($path, $controller);
+
+function addPostPath($path, $controller): void
+{
+    Route::post($path, $controller . '@handle');
+}
+
+Route::get('cities/{id}/delivery-dates-times/{days_count}', 'CityDeliveryDateTimesController@handle');
